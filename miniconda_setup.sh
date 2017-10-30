b74598f90eb223edc24f476e06285623cf9d03c6
Nicholas Youngblut <nicholas.youngblut@tuebingen.mpg.de>
2017-10-30 21:48:49 +0100

    switched comments to echo

--- miniconda_setup.sh
+++ miniconda_setup.sh
@@ -20,69 +20,68 @@ touch $INSTALL_DIR"/DO_NOT_BACKUP_THIS_FOLDER"
 # add conda to PATH
 export PATH=$CONDA_PATH:$PATH
 
-# create conda environemnts
+echo "# Creating conda environemnts"
 conda create -y -n py3 python=3
 conda create -y -n py2 python=2 
 
-# install essential packages into root environment
-
-## jupyter
+echo "# Installing jupyter"
 conda install -y jupyter
 
-## nb_conda
+echo "# Installing nb_conda"
 conda install -y nb_conda
 
-## notebook extensions
-### this next part is needed to avoid "Jupter already running" error
+echo "# Installing  notebook extensions"
+echo "#> this next part is needed to avoid 'Jupter already running' error"
 pip install jupyter_contrib_nbextensions
 jupyter contrib nbextensions install --sys-prefix --skip-running-check
 
-# installing R
+echo "# Installing R-base"
 conda install -y -c r r-base
 conda install -n py2 -y -c r r-base
 conda install -n py3 -y -c r r-base
 
 
-# installing kernels
-## adding kernels for conda environments
+echo "# Installing kernels"
+echo "## Adding kernels for conda environments"
 conda install -y -n py2 ipykernel nb_conda
 conda install -y -n py3 ipykernel nb_conda
 
-## installing rpy2
+echo "## Installing rpy2"
 conda install -y -c r rpy2
 conda install -y -n py2 -c r rpy2
 conda install -y -n py3 -c r rpy2
 
-## installing irkernel
+echo "## Installing irkernel"
 conda install -y -c r r-irkernel
 conda install -y -n py2 -c r r-irkernel
 conda install -y -n py3 -c r r-irkernel
 
 
-# installing env-specific software
-## installing pandas
+echo "# Installing env-specific software"
+echo "## Installing pandas"
 conda install -y -n py2 pandas
 conda install -y -n py3 pandas
 
-## installing qiime
-### QIIME v1
+echo "## Installing QIIME"
+echo "### Installing QIIME v1 in py2 env"
 conda install -y -n py2 -c bioconda qiime
-### QIIME v2
+echo "### Installing QIIME v2 in py3 env"
 conda install -y -n py3 -c qiime2 qiime2
 
 
-# installing R packages
+echo "# Installing R packages via install.packages()"
 Rscript tidyverse_install.R
-## py2
+echo "## Installing R packages in py2 env"
 source activate py2
 #Rscript irkernel_install.R
 Rscript tidyverse_install.R
-## py3
+echo "## Installing R packages in py3 env"
 source activate py3
 #Rscript irkernel_install.R
 Rscript tidyverse_install.R
 
-## nbextension for each environment (in case notebook is started with particular environment)
+echo "## Installing nbextension for each environment"
+echo "##> This is in case notebook is started with particular environment"
 source activate py2
 pip install jupyter_contrib_nbextensions
 jupyter contrib nbextensions install --sys-prefix --skip-running-check
